b2173b1a735b17e6136cd81116e91cd3cf84f702
Gervan <gervan@users.noreply.github.com>
2020-04-07 18:08:20 -0400

    File updates and completion of Features page

--- script.js
+++ script.js
@@ -2,10 +2,6 @@
 
 window.onload = showAlert;
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
 function contactUs() {
   let answer = confirm("Are you sure you want to contact us?");
 
